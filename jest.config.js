0f9a25dd7591122f818c7725aab7fc9aba78243c
pei223 <ippei.223miyake119@gmail.com>
2021-12-16 20:47:29 +0900

    デバッグ実行設定ファイル追加

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-    transform: {
-        ".(ts|tsx)": "ts-jest"
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
     },
-    globals: {
-        "ts-jest": {
-            "tsConfig": "tsconfig.json"
-        }
-    },
-    testMatch: [
-        "**/tests/**/*.test.(ts|tsx)"
-    ],
-    globalSetup: "./tests/setup.ts",
-    globalTeardown: "./tests/teardown.ts",
+  },
+  testMatch: ['**/tests/**/*.test.(ts|tsx)'],
+  globalSetup: './tests/setup.ts',
+  globalTeardown: './tests/teardown.ts',
 }
